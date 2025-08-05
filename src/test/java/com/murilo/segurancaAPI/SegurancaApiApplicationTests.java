e094efd5b65f09b541e40436ba0557c4257f19ca
Murilo Eduardo <muheduardo96@gmail.com>
2025-08-04 21:12:30 -0300

    projeto spring boot inicial

---
+++ src/test/java/com/murilo/segurancaAPI/SegurancaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo.segurancaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurancaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
