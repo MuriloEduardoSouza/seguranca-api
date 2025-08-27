2f84afbae13cec5c55ed494993f165b46c8b443b
Murilo Eduardo <muheduardo96@gmail.com>
2025-08-27 12:14:45 -0300

    Sobe aplicacao para o docker.

--- src/main/java/com/murilo/segurancaAPI/validator/UsuarioValidator.java
+++ src/main/java/com/murilo/segurancaAPI/validator/UsuarioValidator.java
@@ -1,14 +1,12 @@
 package com.murilo.segurancaAPI.validator;
 
-import com.murilo.segurancaAPI.exception.SenhaInvalidaException;
-import com.murilo.segurancaAPI.exception.UsuarioDuplicadoException;
 import com.murilo.segurancaAPI.controller.dto.UsuarioDTO;
 import com.murilo.segurancaAPI.entity.Usuario;
+import com.murilo.segurancaAPI.exception.SenhaInvalidaException;
+import com.murilo.segurancaAPI.exception.UsuarioDuplicadoException;
 import com.murilo.segurancaAPI.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Component
 @RequiredArgsConstructor
